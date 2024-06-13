c33b038d92987cf34e0b0fa2db5870aa464c91bc
GittyNew <153794872+VykuntaBoddepalli@users.noreply.github.com>
2024-06-13 17:02:55 +0530

    commit

--- GPS project/gpio.d
+++
@@ -1,5 +0,0 @@
-.\gpio.o: MCAL\GPIO\GPIO.c
-.\gpio.o: MCAL\GPIO\GPIO.h
-.\gpio.o: .\Utilities\bitManipulation.h
-.\gpio.o: .\Utilities\tm4c123gh6pm.h
-.\gpio.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
--- GPS project/startup_tm4c123.d
+++
@@ -1,1 +0,0 @@
-.\startup_tm4c123.o: Utilities\startup_TM4C123.s
